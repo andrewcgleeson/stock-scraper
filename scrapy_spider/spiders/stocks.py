f8765513abdbfb95c2d6e43ed8db4485d866f73e
Andrew Gleeson <andrew.gleesonc@gmail.com>
2022-02-06 15:43:10 +0000

    Initialized project and set up basic file

---
+++ scrapy_spider/spiders/stocks.py
@@ -0,0 +1,25 @@
+import scrapy
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class StocksSpider(scrapy.Spider):
+    name = "stocks"
+    
+    def start_requests(self):
+        urls = [
+            os.getenv('url')
+        ]
+        for url in urls:
+
+
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        counter = 1
+        for row in response.xpath(f'//*[@id="maincontent"]/div[1]/div[2]/div[2]/div[1]/div/div/div[3]/table/tbody/tr[{counter}]'):
+            yield {
+                'currency_name': row.xpath('td[1]/a/text()').get(),
+                'current_value': row.xpath('td[2]/bg-quote/text()').get(),
+            }
+        counter += 1
