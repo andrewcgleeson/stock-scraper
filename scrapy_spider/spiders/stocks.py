9df7335849c0703baf5caa5106ca5ec9cf7f7cd9
Andrew Gleeson <andrew.gleesonc@gmail.com>
2022-02-08 20:04:49 +0000

    Spider now follows table urls, takes url page data

--- scrapy_spider/spiders/stocks.py
+++ scrapy_spider/spiders/stocks.py
@@ -6,16 +6,21 @@ load_dotenv()
 class StocksSpider(scrapy.Spider):
     name = "stocks"
     
-    def start_requests(self):
-        urls = [
-            os.getenv('urls')
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [os.getenv('urls')]
 
     def parse(self, response):
-        for row in response.xpath(f'//*[@id="maincontent"]/div[1]/div[2]/div[2]/div[1]/div/div/div[3]/table/tbody//tr'):
-            yield {
-                'currency_name': row.xpath('td[1]/a/text()').get(),
-                'current_value': row.xpath('td[2]/bg-quote/text()').get(),
-            }
+        # coin_page_links = response.xpath('//main[@id="maincontent"]/div[1]/div[2]/div[2]/div[1]/div/div/div[3]/table/tbody//tr//a').attrib['href']
+        # yield from response.follow(coin_page_links, callback=self.parse_pages)
+        for href in response.xpath('//main[@id="maincontent"]/div[1]/div[2]/div[2]/div[1]/div/div/div[3]/table/tbody//tr//a/@href'):
+            url=response.urljoin(href.extract())
+            yield scrapy.Request(url, callback = self.parse_pages)
+    
+    def parse_pages(self, response):
+        def extract_information(query):
+            return response.xpath(query).get()
+        
+        yield {
+            'coin_name': extract_information('//*[@id="maincontent"]/div[1]/div[2]/div/div[2]/h1/text()'),
+            'current_price': extract_information('//*[@id="maincontent"]/div[1]/div[3]/div/div[2]/h2/bg-quote/text()'),
+            'day_change': extract_information('//*[@id="maincontent"]/div[1]/div[3]/div/div[2]/bg-quote/span[2]/bg-quote/text()')
+        }
